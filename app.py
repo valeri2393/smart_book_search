7caa75efb3f9265f3fc1297792b7c93c9ea1366d
Valeriya Aleshnikova <160396539+valeri2393@users.noreply.github.com>
2024-07-11 18:15:20 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import pandas as pd
+import torch
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+import faiss
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+
+model_name = "cointegrated/rubert-tiny2"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name)
+
+books = pd.read_csv('data/data_cleaned.csv')
+
+MAX_LEN = 300
+
+def embed_bert_cls(text, model=model, tokenizer=tokenizer):
+    t = tokenizer(text,
+                  padding=True,
+                  truncation=True,
+                  return_tensors='pt',
+                  max_length=MAX_LEN)
+    with torch.no_grad():
+        model_output = model(**{k: v.to(model.device) for k, v in t.items()})
+    embeddings = model_output.last_hidden_state[:, 0, :]
+    embeddings = torch.nn.functional.normalize(embeddings)
+    return embeddings[0].cpu().squeeze()
+
+embeddings = np.loadtxt('models/embeddings.txt')
+embeddings_tensor = [torch.tensor(embedding) for embedding in embeddings]
+
+# Создание индекса Faiss
+embeddings_matrix = np.stack(embeddings)
+index = faiss.IndexFlatIP(embeddings_matrix.shape[1])
+index.add(embeddings_matrix)
+
+st.title('Приложение для рекомендации книг')
+
+text = st.text_input('Введите запрос:')
+num_results = st.number_input('Введите количество рекомендаций:', min_value=1, max_value=50, value=3)
+
+
+# Add a button to trigger the recommendation process
+recommend_button = st.button('Получить рекомендации')
+
+if text and recommend_button:  # Check if the user entered text and clicked the button
+
+    # Встраивание запроса и поиск ближайших векторов с использованием Faiss
+    query_embedding = embed_bert_cls(text)
+    query_embedding = query_embedding.numpy().astype('float32')
+    _, indices = index.search(np.expand_dims(query_embedding, axis=0), num_results)
+
+    st.subheader('Топ рекомендуемых книг:')
+    for i in indices[0]:
+        recommended_embedding = embeddings_tensor[i].numpy()  # Вектор рекомендованной книги
+        similarity = np.dot(query_embedding, recommended_embedding)  # Косинусное сходство
+        similarity_percent = similarity * 100
+        
+        col1, col2 = st.columns([1, 3])
+        
+        with col1:
+            st.image(books['image_url'][i], use_column_width=True)
+        
+        with col2:
+            st.write(f"**Название книги:** {books['title'][i]}")
+            st.write(f"**Автор:** {books['author'][i]}")
+            st.write(f"**Описание:** {books['annotation'][i]}")
+            st.write(f"**Оценка сходства:** {similarity_percent:.2f}%")
+        
+        st.write("---")
