a3f2354448dafbafb52e938b65618c466b5ee1b9
Valeriya Aleshnikova <160396539+valeri2393@users.noreply.github.com>
2024-07-12 14:36:36 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ import torch
 import numpy as np
 from transformers import AutoTokenizer, AutoModel
 import faiss
+from streamlit.errors import StreamlitAPIException
+import urllib.parse
+
+
 
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
-
-model_name = "cointegrated/rubert-tiny2"
+# Load model and tokenizer
+model_name = "sentence-transformers/msmarco-distilbert-base-v3"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
-books = pd.read_csv('data/data_cleaned.csv')
+# Load data
+books = pd.read_csv('data/data_final_version.csv')
 
 MAX_LEN = 300
 
@@ -29,45 +34,102 @@ def embed_bert_cls(text, model=model, tokenizer=tokenizer):
     embeddings = torch.nn.functional.normalize(embeddings)
     return embeddings[0].cpu().squeeze()
 
+# Load embeddings
 embeddings = np.loadtxt('models/embeddings.txt')
 embeddings_tensor = [torch.tensor(embedding) for embedding in embeddings]
 
-# Создание индекса Faiss
+# Create Faiss index
 embeddings_matrix = np.stack(embeddings)
 index = faiss.IndexFlatIP(embeddings_matrix.shape[1])
 index.add(embeddings_matrix)
 
-st.title('Приложение для рекомендации книг')
 
-text = st.text_input('Введите запрос:')
-num_results = st.number_input('Введите количество рекомендаций:', min_value=1, max_value=50, value=3)
+# CSS стили для заднего фона
+background_image = """
+    <style>
+    .stApp {
+        background-image: url("https://img.freepik.com/premium-photo/blur-image-book_9563-1100.jpg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    </style>
+"""
+
+# Вставляем CSS стили в приложение Streamlit
+st.markdown(background_image, unsafe_allow_html=True)
 
 
-# Add a button to trigger the recommendation process
+# Вставляем CSS стили для окошка с прозрачным фоном
+transparent_title = """
+    <style>
+    .transparent-title {
+        background-color: rgba(255, 255, 255, 0.7);
+        padding: 10px;
+        border-radius: 5px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    }
+    </style>
+"""
+
+transparent_box = """
+    <style>
+    .transparent-box {
+        background-color: rgba(255, 255, 255, 0.7);
+        padding: 10px;
+        border-radius: 5px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    }
+    </style>
+"""
+
+# Вставляем CSS стили в приложение Streamlit
+st.markdown(transparent_title, unsafe_allow_html=True)
+st.markdown(transparent_box, unsafe_allow_html=True)
+
+# Streamlit interface
+st.markdown('<h1 class="transparent-title">🎓📚Приложение для рекомендаций книг📚🎓</h1>', unsafe_allow_html=True)
+
+# Далее ваш код Streamlit
+text = st.text_input('Введите ваш запрос для поиска книг:')
+num_results = st.number_input('Количество результатов:', min_value=1, max_value=20, value=3)
 recommend_button = st.button('Получить рекомендации')
+
 
 if text and recommend_button:  # Check if the user entered text and clicked the button
 
-    # Встраивание запроса и поиск ближайших векторов с использованием Faiss
+    # Embed the query and search for nearest vectors using Faiss
     query_embedding = embed_bert_cls(text)
     query_embedding = query_embedding.numpy().astype('float32')
     _, indices = index.search(np.expand_dims(query_embedding, axis=0), num_results)
 
-    st.subheader('Топ рекомендуемых книг:')
+    st.subheader('Рекомендации по вашему запросу:')
     for i in indices[0]:
-        recommended_embedding = embeddings_tensor[i].numpy()  # Вектор рекомендованной книги
-        similarity = np.dot(query_embedding, recommended_embedding)  # Косинусное сходство
+        recommended_embedding = embeddings_tensor[i].numpy()  # Vector of the recommended book
+        similarity = np.dot(query_embedding, recommended_embedding) / (np.linalg.norm(query_embedding) * np.linalg.norm(recommended_embedding))  # Cosine similarity
         similarity_percent = similarity * 100
-        
+
         col1, col2 = st.columns([1, 3])
-        
         with col1:
-            st.image(books['image_url'][i], use_column_width=True)
-        
+            image_url = books['image_url'][i]
+            if pd.isna(image_url) or not image_url or image_url.strip() == '':
+                st.write("Обложка не найдена")
+            else:
+                try:
+                    st.image(image_url, use_column_width=True)
+                except Exception as e:
+                    st.write("Обложка не найдена")
+                    st.write(e)
+
         with col2:
-            st.write(f"**Название книги:** {books['title'][i]}")
-            st.write(f"**Автор:** {books['author'][i]}")
-            st.write(f"**Описание:** {books['annotation'][i]}")
-            st.write(f"**Оценка сходства:** {similarity_percent:.2f}%")
-        
+            # Выводим информацию о книге на прозрачном фоне
+            st.markdown(f"""
+                <div class="transparent-box">
+                    <p><b>Название книги:</b> {books['title'][i]}</p>
+                    <p><b>Автор:</b> {books['author'][i]}</p>
+                    <p><b>Описание:</b>{books['annotation'][i]}")
+                    <p><b>Оценка сходства:</b> {similarity_percent:.2f}%</p>
+                </div>
+            """, unsafe_allow_html=True)
+
         st.write("---")
